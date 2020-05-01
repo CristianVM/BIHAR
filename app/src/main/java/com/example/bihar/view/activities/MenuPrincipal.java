a53f5899c5c80d14ded628e575151086705d75a4
IkNafa <PoKemon1000>
2020-05-01 18:04:31 +0200

    Menu más bonito

--- app/src/main/java/com/example/bihar/view/activities/MenuPrincipal.java
+++ app/src/main/java/com/example/bihar/view/activities/MenuPrincipal.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.bihar.model.CardOpcion;
@@ -116,7 +117,7 @@ public class MenuPrincipal extends AppCompatActivity {
 
 class MiViewHolder extends RecyclerView.ViewHolder{
 
-    public ImageButton imgButton;
+    public ImageView imgButton;
 
     public MiViewHolder(@NonNull View itemView) {
         super(itemView);
