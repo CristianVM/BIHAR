e3de47bba3b71e51bd4ef408a56ec17ca2198dcc
Cristian <cris.vm33@gmail.com>
2020-05-05 15:12:28 +0200

    Inicio sesión general (Con fusión e imagen) + Icono app + Firebase token

--- app/src/main/java/com/example/bihar/controller/WorkerBihar.java
+++ app/src/main/java/com/example/bihar/controller/WorkerBihar.java
@@ -1,16 +1,32 @@
 package com.example.bihar.controller;
 
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
+import android.util.Base64;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -40,24 +56,43 @@ public class WorkerBihar extends Worker {
 
             int statusCode = urlConnection.getResponseCode();
             if (statusCode == 200) {
-                BufferedInputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-                String line;
-                String result="";
-                while ((line = bufferedReader.readLine()) != null) {
-                    result += line;
-                }
-                inputStream.close();
+                JSONParser parser = new JSONParser();
+                JSONObject json = (JSONObject) parser.parse(jsonString);
+                if (json.get("accion").equals("obtenerImagen")) {
+                    // Transformamos el resultado de la llamada en un Bitmap
+                    Bitmap bitmap = BitmapFactory.decodeStream(urlConnection.getInputStream());
+                    File directorio = getApplicationContext().getFilesDir();
+                    File.createTempFile("perfil", ".png", directorio);
 
-                if(!result.isEmpty()){
+                    File imagenFichero = new File(getApplicationContext().getFilesDir(), "perfil" + ".png");
+                    OutputStream os = new FileOutputStream(imagenFichero);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
+                    os.flush();
+                    os.close();
+
                     Data.Builder data = new Data.Builder();
-
-                    data.putString("result",result);
-                    Log.i("BDD",result);
-
+                    data.putString("result", "OK");
                     return Result.success(data.build());
-                }else{
-                    Log.i("BDD","Vacio");
+                } else {
+                    BufferedInputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                    String line;
+                    String result="";
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result += line;
+                    }
+                    inputStream.close();
+
+                    if(!result.isEmpty()){
+                        Data.Builder data = new Data.Builder();
+
+                        data.putString("result",result);
+                        Log.i("BDD",result);
+
+                        return Result.success(data.build());
+                    }else{
+                        Log.i("BDD","Vacio");
+                    }
                 }
             }else{
                 Log.i("BDD",statusCode+"");
@@ -66,6 +101,8 @@ public class WorkerBihar extends Worker {
             e.printStackTrace();
         }
 
-        return Result.failure();
+        Data.Builder data = new Data.Builder();
+        data.putString("result","FAIL");
+        return Result.failure(data.build());
     }
 }
