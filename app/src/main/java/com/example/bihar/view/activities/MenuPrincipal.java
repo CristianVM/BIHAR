9a57511dac5bfc5f6c4a9e375b6bac97166b4f28
IkNafa <PoKemon1000>
2020-05-09 15:25:47 +0200

    Fondo verde en el menu principal

--- app/src/main/java/com/example/bihar/view/activities/MenuPrincipal.java
+++ app/src/main/java/com/example/bihar/view/activities/MenuPrincipal.java
@@ -1,31 +1,28 @@
 package com.example.bihar.view.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bihar.R;
 import com.example.bihar.model.CardOpcion;
 import com.example.bihar.model.ListaOpcionesMenu;
-import com.example.bihar.R;
 
 public class MenuPrincipal extends AppCompatActivity {
 
